ebc4256531dd8504ea3bf3a1890f4a6cabfe7689
Daoi <36860391+Daoi@users.noreply.github.com>
2020-04-18 01:06:16 -0400

    Extended Button class for letter tile game piece

---
+++ LetterTile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrabble
+{
+    public class LetterTile:Button
+    {
+        string tileLetter { get; set; }
+        string tileScore { get; set; }
+        bool isBlankTile { get; set; } = false;
+        int location { get; set; }
+
+    public LetterTile(string letter, string score)
+        {
+            if (letter.Equals(" "))
+                isBlankTile = true;
+
+            tileLetter = letter;
+            tileScore = score;
+            base.Text = tileLetter;
+        }
+
+         
+    }
+}
